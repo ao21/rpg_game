7fe02a843be89e2e40cc51100daf5bb1fc3df728
ao21 <lpubnrm.ay@gmail.com>
2021-02-03 12:44:49 +0900

    Characterクラスのinitializeの引数をオプション引数に変更

--- character.rb
+++ character.rb
@@ -1,7 +1,7 @@
 class Character
   attr_reader :name, :offense, :defense
   attr_accessor :hp
-  def initialize(params)
+  def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
